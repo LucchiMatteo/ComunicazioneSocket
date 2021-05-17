e6410bfa55976322bf5a7f63def30dbf8da24d56
Francesco Foschi <79740595+Francesco08-bit@users.noreply.github.com>
2021-05-17 15:09:25 +0200

    Comunicazione Socket.

--- ComunicazioneSocket/MainWindow.xaml.cs
+++ ComunicazioneSocket/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//aggiunta
+//Aggiunta.
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -24,37 +24,47 @@ namespace ComunicazioneSocket
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Programmazione per la comunicazione fra Socket.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
 
-            IPEndPoint localendpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 56000);
+            //Rappresenta un punto di rete sotto forma di un indirizzo IP e di un numero di porta in questo caso l'ip local.
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 56000);
 
+            //Thread.
             Thread t1 = new Thread(new ParameterizedThreadStart(SocketReceive));
 
-            t1.Start(localendpoint);
-
-
+            //Si fa startare il Thread con al suo interno il localEndPoint.
+            t1.Start(localEndPoint);
         }
 
         public async void SocketReceive (object sourceEndPoint)
         {
+            //Rappresenta un punto di rete sotto forma di un indirizzo IP e di un numero di porta in questo caso l'ip del mittente.
             IPEndPoint sourceEP = (IPEndPoint)sourceEndPoint;
 
+            //Socket.
             Socket t = new Socket(sourceEP.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
 
+            //Instaura la connessione.
             t.Bind(sourceEP);
 
+            //Byte ricevuti.
             Byte[] byteRicevuti = new byte[256];
             string message = "";
 
             int bytes = 0;
 
+            //Metodo che permette a l'altro processo di andare normalmente.
             await Task.Run(() =>
             {
                 while (true)
                 {
-                    if(t.Available>0)
+                    //Se il Thread è maggiore di 0 il messaggio verrà inviato.
+                    if (t.Available>0)
                     {
                         message = "";
                         bytes = t.Receive(byteRicevuti, byteRicevuti.Length, 0);
@@ -65,22 +75,23 @@ namespace ComunicazioneSocket
                             lblRicezione.Content = message;
                         }));
                     }
-
-
                 }
-
             });
         }
 
         private void btnInvia_Click(object sender, RoutedEventArgs e)
         {
+            //IPAddress del destinatario.
             IPAddress ipDest = IPAddress.Parse(txtIpAdd.Text);
             int portDest = int.Parse(txtDestPort.Text);
 
+            //Punto remoto.
             IPEndPoint remoteEndPoint = new IPEndPoint(ipDest, portDest);
 
+            //Socket.
             Socket s = new Socket(ipDest.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
 
+            //Byte.
             Byte[] byteInviati = Encoding.ASCII.GetBytes(txtMsg.Text);
 
             s.SendTo(byteInviati, remoteEndPoint);
